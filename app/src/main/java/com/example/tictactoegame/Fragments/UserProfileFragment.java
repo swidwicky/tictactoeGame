3adb77170df1fa6af0dedd868b71ca921b69eafe
shareef <shareefmahomed98@gmail.com>
2023-09-19 18:13:17 +0800

    made the basic design for the user profile

--- app/src/main/java/com/example/tictactoegame/Fragments/UserProfileFragment.java
+++ app/src/main/java/com/example/tictactoegame/Fragments/UserProfileFragment.java
@@ -10,57 +10,64 @@ import android.view.ViewGroup;
 
 import com.example.tictactoegame.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link UserProfileFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 public class UserProfileFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private RecyclerView avatarRecyclerView;
+    private EditText userNameEditText;
+    private Button saveButton;
+    private Button backButton;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public UserProfileFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment UserProfileFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static UserProfileFragment newInstance(String param1, String param2) {
-        UserProfileFragment fragment = new UserProfileFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
+    @Nullable
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_user_profile, container, false);
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user_profile, container, false);
+        // Initialize UI elements
+        userNameEditText = view.findViewById(R.id.userNameEditText);
+        avatarRecyclerView = view.findViewById(R.id.avatarSelectionRecyclerView);
+        saveButton = view.findViewById(R.id.saveButton);
+        backButton = view.findViewById(R.id.backButton);
+
+        // Set up RecyclerView
+        AvatarAdapter adapter = new AvatarAdapter(getContext(), avatarImages); // Replace avatarImages with your avatar data source.
+        avatarRecyclerView.setAdapter(adapter);
+        avatarRecyclerView.setLayoutManager(new GridLayoutManager(getContext(), 3)); // 3 columns
+
+        // Set click listeners for buttons
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle save button click
+            }
+        });
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle back button click
+                FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
+                Fragment mainMenuFragment = new MainMenuFragment(); // Replace with the actual name of your main_menu fragment class.
+                fragmentManager.beginTransaction()
+                        .replace(R.id.UserProfileContainer, mainMenuFragment)
+                        .commit();
+            }
+        });
+
+
+        return view;
     }
 }
