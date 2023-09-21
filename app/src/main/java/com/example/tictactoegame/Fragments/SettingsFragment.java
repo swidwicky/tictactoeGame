9586defb70b1bf16458774abc12b85d6028bdc2d
shareef <shareefmahomed98@gmail.com>
2023-09-21 20:21:01 +0800

    made the settings fragment, the WinConditions Fragment and the Player marker fragment and linked them to their buttons

--- app/src/main/java/com/example/tictactoegame/Fragments/SettingsFragment.java
+++ app/src/main/java/com/example/tictactoegame/Fragments/SettingsFragment.java
@@ -1,66 +1,111 @@
-package com.example.tictactoegame.Fragments;
+package com.example.tictactoegame.Fragments; // Replace with your actual package name
 
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.tictactoegame.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SettingsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class SettingsFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    // Define the View elements
+    private TextView settingsTextView;
+    private Button gameBoardSettButton;
+    private Button winConditionsButton;
+    private Button playerMarkerButton;
+    private Button backButton;
+    private Button homeButton;
 
     public SettingsFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SettingsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SettingsFragment newInstance(String param1, String param2) {
-        SettingsFragment fragment = new SettingsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_settings, container, false);
+
+        // Initialize the View elements
+        settingsTextView = view.findViewById(R.id.SettingsTextView);
+        gameBoardSettButton = view.findViewById(R.id.GameBoardSettingsButton);
+        winConditionsButton = view.findViewById(R.id.WinConditionsButton);
+        playerMarkerButton = view.findViewById(R.id.playerMarkerButton);
+        backButton = view.findViewById(R.id.backButton);
+        homeButton = view.findViewById(R.id.homeButton);
+        gameBoardSettButton.setOnClickListener(new View.OnClickListener() {
+           public void onClick(View v) {
+               // Handle Win Conditions button click
+               // You can navigate to the WinConditionsFragment or perform the desired action here
+               // Create an instance of the WinConditionsFragment
+               GameFragment gameFragment = new GameFragment();
+
+               FragmentTransaction transaction = getParentFragmentManager().beginTransaction();
+
+               // Replace the current fragment with WinConditionsFragment
+               transaction.replace(R.id.fragment_container, gameFragment);
+               transaction.addToBackStack(null);
+               // Commit the transaction
+               transaction.commit();
+           }
+       });
+
+        winConditionsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle Win Conditions button click
+                // You can navigate to the WinConditionsFragment or perform the desired action here
+                // Create an instance of the WinConditionsFragment
+                WinConditionsFragment winConditionsFragment = new WinConditionsFragment();
+
+                FragmentTransaction transaction = getParentFragmentManager().beginTransaction();
+
+                // Replace the current fragment with WinConditionsFragment
+                transaction.replace(R.id.fragment_container, winConditionsFragment);
+                transaction.addToBackStack(null);
+                // Commit the transaction
+                transaction.commit();
+            }
+        });
+
+
+        playerMarkerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle PLAYER MARKER Conditions button click
+                // Create an instance of the WinConditionsFragment
+                PlayerMarkerFragment playerMarkerFrag = new PlayerMarkerFragment();
+
+                FragmentTransaction transaction = getParentFragmentManager().beginTransaction();
+
+                // Replace the current fragment with WinConditionsFragment
+                transaction.replace(R.id.fragment_container, playerMarkerFrag);
+                transaction.addToBackStack(null);
+                // Commit the transaction
+                transaction.commit();
+            }
+        });
+
+        backButton.setOnClickListener(v -> {
+            navigateBack(); // Custom method to handle fragment navigation
+        });
+
+        homeButton.setOnClickListener( v -> {
+            navigateBack();
+        });
+
+        return view;
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+    private void navigateBack() {
+        // Replace the current fragment with the main menu fragment
+        MainMenuFragment mainMenuFragment = new MainMenuFragment();
+        FragmentTransaction transaction = getParentFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_container, mainMenuFragment);
+        transaction.commit();
     }
 }
